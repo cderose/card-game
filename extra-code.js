3bb33c7f89474d665024398741fba53b952995fb
Catherine DeRose <catherinederose@Catherines-MacBook-Pro.local>
2021-02-09 20:50:49 -0600

    select random cards

--- extra-code.js
+++ extra-code.js
@@ -1,1 +1,20 @@
 //document.getElementById('test').src='/images/cards/2H.png'
+
+// var path = 'images/cards/',
+// cards = [
+//   '2C.png', '2D.png', '2H.png', '2S.png',
+//   '3C.png', '3D.png', '2H.png', '2S.png',
+//   '4C.png', '4D.png', '2H.png', '2S.png',
+//   '5C.png', '5D.png', '2H.png', '2S.png',
+//   '6C.png', '6D.png', '2H.png', '2S.png',
+//   '7C.png', '7D.png', '2H.png', '2S.png',
+//   '8C.png', '8D.png', '2H.png', '2S.png',
+//   '9C.png', '9D.png', '2H.png', '2S.png',
+//   '10C.png', '1D.png', '2H.png', '2S.png',
+//   'JC.png', 'JD.png', '2H.png', '2S.png',
+//   'QC.png', '2D.png', '2H.png', '2S.png',
+//   'KC.png', '2D.png', '2H.png', '2S.png',
+//   'AC.png', '2D.png', '2H.png', '2S.png',
+// ]
+// var randomNumber = Math.floor(Math.random() * cards.length);
+// var randomCard = path + cards[randomNumber]
--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+var path = 'images/cards/',
+suits = ['C.png', 'D.png', 'H.png', 'S.png']
+var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
+var randomSuit = Math.floor(Math.random() * 4)
+var randomCard = path + randomCardNumber + suits[randomSuit];
+
+// CLICK A BUTTON TO PLAY
 for (i=0; i < document.querySelectorAll(".button").length; i++) {
   document.querySelectorAll(".button")[i].addEventListener("click", function() {
     var buttonText = this.textContent;
@@ -8,10 +15,11 @@ for (i=0; i < document.querySelectorAll(".button").length; i++) {
 function flipCard(key){
   switch (key) {
     case "Turn Card":
-      document.getElementsByClassName('rcard')[0].src="images/cards/2H.png"
+      document.getElementsByClassName('rcard')[0].src=randomCard
+      console.log(randomCard)
       break;
     case "Pass":
-      document.getElementsByClassName('bcard')[0].src="images/cards/2H.png"
+      document.getElementsByClassName('bcard')[0].src=randomCard
       break;
     default: console.log(this.textContent);
   }
