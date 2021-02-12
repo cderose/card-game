7ebcba9fa2b2a173f4ced867ca94a2575aa7248d
Catherine DeRose <catherinederose@Catherines-MacBook-Pro.local>
2021-02-11 18:58:28 -0600

    set up computer automation

--- game.js
+++ game.js
@@ -7,48 +7,68 @@ var bcard = 0
 for (i=0; i < document.querySelectorAll(".button").length; i++) {
   document.querySelectorAll(".button")[i].addEventListener("click", function() {
     var buttonText = this.textContent;
-    if (buttonText != "Start Over"){
-      flipCard(buttonText);
+    if (buttonText == "Turn Card"){
+      turnCard(buttonText);
+    } else if (buttonText == "Pass Turn"){
+      passTurn(buttonText)
     } else {
       startGame();
     }
   })
 }
 
-function flipCard(options){
+function turnCard(){
   var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
   var randomSuit = Math.floor(Math.random() * 4);
   var randomCard = path + randomCardNumber + suits[randomSuit];
-  switch (options) {
-    case "Turn Card":
-      if (rcard < 4){
-        document.getElementsByClassName('rcard')[rcard].src=randomCard;
-        rcard += 1;
-      } else {
-        document.getElementsByClassName('rcard')[rcard].src=randomCard;
-        endGame();
-      }
-      break;
-    case "Pass":
-    if (bcard < 4){
-      document.getElementsByClassName('bcard')[bcard].src=randomCard;
-      bcard += 1;
-    } else {
-      document.getElementsByClassName('bcard')[bcard].src=randomCard;
-      endGame();
-    }
-    break;
+  if (rcard < 4){
+    document.getElementsByClassName('rcard')[rcard].src=randomCard;
+    rcard += 1;
+  } else {
+    document.getElementsByClassName('rcard')[rcard].src=randomCard;
+    endGame();
   }
 }
 
+function passTurn(){
+  document.getElementsByClassName('turn-card')[0].style.display = "none";
+  document.getElementsByClassName('pass-turn')[0].style.display = "none";
+  var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
+  var randomSuit = Math.floor(Math.random() * 4);
+  var randomCard = path + randomCardNumber + suits[randomSuit];
+  if (bcard < 4){
+    document.getElementsByClassName('bcard')[bcard].src=randomCard;
+    bcard += 1;
+    compChoice();
+  } else {
+    document.getElementsByClassName('bcard')[bcard].src=randomCard;
+    endGame();
+  }
+}
+
+function compChoice() {
+  var compRandom = Math.floor(Math.random() * 2);
+  if (compRandom === 0) {
+    setTimeout(function(){
+      passTurn(); }, 3000);
+  } else {
+    document.getElementsByClassName('turn-card')[0].style.display = "inline-block";
+    document.getElementsByClassName('pass-turn')[0].style.display = "inline-block";
+  }
+}
+
+
+
+
 // END GAME AND START OVER //
 function endGame(){
-  document.getElementsByClassName('turn-card')[0].innerText = "Start Over";
+  document.getElementsByClassName('new-game')[0].style.display = "inline-block";
   document.getElementsByClassName('pass-turn')[0].style.display = "none";
 }
 
 function startGame(){
-  document.getElementsByClassName('turn-card')[0].innerText = "Turn Card";
+  document.getElementsByClassName('new-game')[0].style.display = "none";
+  document.getElementsByClassName('turn-card')[0].style.display = "inline-block";
   document.getElementsByClassName('pass-turn')[0].style.display = "inline-block";
   rcard = 0;
   bcard = 0;
