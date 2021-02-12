02b685bbbfb2fad14196afc803b346e9fa5b065e
Catherine DeRose <catherinederose@Catherines-MacBook-Pro.local>
2021-02-11 21:45:20 -0600

    add computer logic

--- game.js
+++ game.js
@@ -8,12 +8,12 @@ suits = ['C.png', 'D.png', 'H.png', 'S.png']
 
 
 // GAMEPLAY //
-for (i=0; i < document.querySelectorAll(".button").length; i++) {
+for (i = 0; i < document.querySelectorAll(".button").length; i++) {
   document.querySelectorAll(".button")[i].addEventListener("click", function() {
     var buttonText = this.textContent;
-    if (buttonText == "Higher Card"){
+    if (buttonText == "Higher Card") {
       humanHigher(buttonText);
-    } else if (buttonText == "Lower Card"){
+    } else if (buttonText == "Lower Card") {
       humanLower(buttonText);
     } else if (buttonText == "Change Card") {
       compTurn(buttonText)
@@ -23,10 +23,10 @@ for (i=0; i < document.querySelectorAll(".button").length; i++) {
   })
 }
 
-function humanHigher(){
+function humanHigher() {
   var randomCard = drawnCard();
-  document.getElementsByClassName('rcard')[rcard].src=randomCard[0];
-  console.log(lastRed);
+  document.getElementsByClassName('rcard')[rcard].src = randomCard[0];
+  //console.log(lastRed);
   if (randomCard[1] > lastRed) {
     if (rcard < 4) {
       rcard += 1;
@@ -36,16 +36,18 @@ function humanHigher(){
     }
   } else {
     setTimeout(function() {
-      document.getElementsByClassName('rcard')[rcard].src="images/red-cards.jpg"; compTurn();}, 1500);
+      document.getElementsByClassName('rcard')[rcard].src = "images/red-cards.jpg";
+      compTurn();
+    }, 1500);
   }
 }
 
-function humanLower(){
+function humanLower() {
   var randomCard = drawnCard();
-  document.getElementsByClassName('rcard')[rcard].src=randomCard[0];
-  console.log(lastRed);
+  document.getElementsByClassName('rcard')[rcard].src = randomCard[0];
+  //console.log(lastRed);
   if (randomCard[1] < lastRed) {
-    if (rcard < 4){
+    if (rcard < 4) {
       rcard += 1;
       lastRed = randomCard[1];
     } else {
@@ -53,83 +55,115 @@ function humanLower(){
     }
   } else {
     setTimeout(function() {
-      document.getElementsByClassName('rcard')[rcard].src="images/red-cards.jpg"; compTurn();}, 1500);
+      document.getElementsByClassName('rcard')[rcard].src = "images/red-cards.jpg";
+      compTurn();
+    }, 1500);
   }
 }
 
-function compTurn(){
-  document.getElementById('comp-higher').style.display = "inline-block";
+function compTurn() {
   document.getElementsByClassName('higher-card')[0].style.display = "none";
   document.getElementsByClassName('lower-card')[0].style.display = "none";
-  // document.getElementsByClassName('pass-turn')[0].style.display = "none";
-  var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
-  var randomSuit = Math.floor(Math.random() * 4);
-  var randomCard = path + randomCardNumber + suits[randomSuit];
-  setTimeout(function(){
-    if (bcard < 4){
-      document.getElementsByClassName('bcard')[bcard].src=randomCard;
-      bcard += 1;
-      compChoice();
-    } else {
-      document.getElementsByClassName('bcard')[bcard].src=randomCard;
-      endGame();
-    }; }, 1500);
+  var compChoice = Math.floor(Math.random() * 2);
+  if (compChoice === 0) {
+    compHigher();
+  } else {
+    compLower();
+  }
 }
 
-function compChoice() {
-  var compRandom = Math.floor(Math.random() * 2);
-  if (compRandom === 0) {
-    setTimeout(function(){
-      compTurn(); }, 1500);
+function compHigher() {
+  document.getElementById('comp-higher').style.display = "inline-block";
+  var randomCard = drawnCard();
+  document.getElementsByClassName('bcard')[bcard].src = randomCard[0];
+  console.log(lastBlue);
+  if (randomCard[1] > lastBlue) {
+     setTimeout(function(){
+       if (bcard < 4) {
+         bcard += 1;
+         lastBlue = randomCard[1];
+         document.getElementById('comp-higher').style.display = "none";
+         compTurn();
+       } else {
+         endGame();
+       }
+     }, 1500);
   } else {
-    document.getElementById('comp-higher').style.display = "none";
+    setTimeout(function() {
+      document.getElementsByClassName('bcard')[bcard].src = "images/blue-cards.jpg";
+      document.getElementById('comp-higher').style.display = "none";
+      document.getElementsByClassName('higher-card')[0].style.display = "inline-block";
+      document.getElementsByClassName('lower-card')[0].style.display = "inline-block";
+    }, 1500);
+  }
+}
+
+function compLower() {
+  document.getElementById('comp-lower').style.display = "inline-block";
+  var randomCard = drawnCard();
+  document.getElementsByClassName('bcard')[bcard].src = randomCard[0];
+  console.log(lastBlue);
+  if (randomCard[1] < lastBlue) {
+     setTimeout(function(){
+       if (bcard < 4) {
+         bcard += 1;
+         lastBlue = randomCard[1];
+         document.getElementById('comp-lower').style.display = "none";
+         compTurn();
+       } else {
+         endGame();
+       }
+     }, 1500);
+  } else {
+    setTimeout(function() {
+      document.getElementsByClassName('bcard')[bcard].src = "images/blue-cards.jpg";
+      document.getElementById('comp-lower').style.display = "none";
+      document.getElementsByClassName('higher-card')[0].style.display = "inline-block";
+      document.getElementsByClassName('lower-card')[0].style.display = "inline-block";
+    }, 1500);
+  }
+}
+
+
+
+  // START AND END SETTINGS //
+  function drawnCard() {
+    var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
+    var randomSuit = Math.floor(Math.random() * 4);
+    var randomCard = path + randomCardNumber + suits[randomSuit];
+    return [randomCard, randomCardNumber];
+  }
+
+  function startGame() {
+    document.getElementsByClassName('new-game')[0].style.display = "none";
     document.getElementsByClassName('higher-card')[0].style.display = "inline-block";
     document.getElementsByClassName('lower-card')[0].style.display = "inline-block";
     // document.getElementsByClassName('pass-turn')[0].style.display = "inline-block";
+    rcard = 0;
+    bcard = 0;
+    for (i = 0; i < document.querySelectorAll(".rcard").length; i++) {
+      document.querySelectorAll(".rcard")[i].src = "images/red-cards.jpg";
+    }
+    for (i = 0; i < document.querySelectorAll(".bcard").length; i++) {
+      document.querySelectorAll(".bcard")[i].src = "images/blue-cards.jpg";
+    }
+    var randomRedCard = drawnCard();
+
+    // draw starting cards
+    document.getElementsByClassName('rcard')[rcard].src = randomRedCard[0];
+    rcard += 1;
+    lastRed = randomRedCard[1]
+    var randomBlueCard = drawnCard();
+    document.getElementsByClassName('bcard')[bcard].src = randomBlueCard[0];
+    bcard += 1;
+    lastBlue = randomBlueCard[1]
   }
-}
 
-
-// START AND END SETTINGS //
-function drawnCard(){
-  var randomCardNumber = Math.floor(Math.random() * (15 - 2) + 2);
-  var randomSuit = Math.floor(Math.random() * 4);
-  var randomCard = path + randomCardNumber + suits[randomSuit];
-  return [randomCard, randomCardNumber];
-}
-
-function startGame(){
-  document.getElementsByClassName('new-game')[0].style.display = "none";
-  document.getElementsByClassName('higher-card')[0].style.display = "inline-block";
-  document.getElementsByClassName('lower-card')[0].style.display = "inline-block";
-  // document.getElementsByClassName('pass-turn')[0].style.display = "inline-block";
-  rcard = 0;
-  bcard = 0;
-  for (i=0; i < document.querySelectorAll(".rcard").length; i++) {
-    document.querySelectorAll(".rcard")[i].src="images/red-cards.jpg";
+  function endGame() {
+    document.getElementById('comp-higher').style.display = "none"
+    document.getElementById('comp-lower').style.display = "none"
+    document.getElementsByClassName('higher-card')[0].style.display = "none";
+    document.getElementsByClassName('lower-card')[0].style.display = "none";
+    // document.getElementsByClassName('pass-turn')[0].style.display = "none";
+    document.getElementsByClassName('new-game')[0].style.display = "inline-block";
   }
-  for (i=0; i < document.querySelectorAll(".bcard").length; i++) {
-    document.querySelectorAll(".bcard")[i].src="images/blue-cards.jpg";
-  }
-  var randomRedCard = drawnCard();
-
-  // draw starting cards
-  document.getElementsByClassName('rcard')[rcard].src=randomRedCard[0];
-  rcard += 1;
-  lastRed = randomRedCard[1]
-  var randomBlueCard = drawnCard();
-  document.getElementsByClassName('bcard')[bcard].src=randomBlueCard[0];
-  bcard += 1;
-  lastBlue = randomRedCard[1]
-
-}
-
-function endGame(){
-  document.getElementById('comp-higher').style.display = "none"
-  document.getElementById('comp-lower').style.display = "none"
-  document.getElementsByClassName('higher-card')[0].style.display = "none";
-  document.getElementsByClassName('lower-card')[0].style.display = "none";
-  // document.getElementsByClassName('pass-turn')[0].style.display = "none";
-  document.getElementsByClassName('new-game')[0].style.display = "inline-block";
-
-}
